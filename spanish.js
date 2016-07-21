37ef7f5fd100451e6149c62a8b07c2c837ca7954
Edgar Barajas <ebarajas05@gmail.com>
2016-07-21 11:23:23 -0500

    added files touched

---
+++ spanish.js
@@ -0,0 +1,19 @@
+console.log(Team JEZ Translator)
+
+var  = (function() {
+  var color = "blue";
+  var type = "sedan";
+
+  return {
+    manufacture_date: new Date(),
+    getColor: function() {
+      return color;
+    },
+    setColor: function(newColor) {
+      color = newColor;
+    },
+    getType: function() {
+      return type;
+    }
+  };
+})();
